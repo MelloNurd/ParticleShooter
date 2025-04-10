0a0399040c04d56939c6e140d4c206630c9cdebe
Ben Bonus <jbenbonus@gmail.com>
2025-04-10 14:48:14 -0700

    Changed line to ship to just an arrow

--- Assets/Scripts/Homebase.cs
+++ Assets/Scripts/Homebase.cs
@@ -5,6 +5,7 @@ using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Homebase : MonoBehaviour
 {
@@ -12,11 +13,9 @@ public class Homebase : MonoBehaviour
 
     public Vector3 HomePos => transform.position;
 
-    private LineRenderer _playerBaseLine;
-    private Material _lineMaterial;
     private TMP_Text _distanceText;
-
-    [SerializeField] private GameObject _test;
+    private GameObject _outlinePivot;
+    private Image _distanceOutline;
 
     public static UnityEvent EnterHomebase = new();
     private void Awake()
@@ -31,57 +30,32 @@ public class Homebase : MonoBehaviour
             Destroy(gameObject);
         }
 
-        _playerBaseLine = transform.Find("Trail").GetComponent<LineRenderer>();
         _distanceText = transform.Find("UI").GetComponentInChildren<TMP_Text>();
-        _lineMaterial = _playerBaseLine.material;
-        _playerBaseLine.SetPosition(0, transform.position);
+        _outlinePivot = _distanceText.transform.GetChild(0).gameObject;
+        _distanceOutline = _outlinePivot.transform.GetChild(0).GetComponent<Image>();
 
         GetComponentInChildren<Canvas>().worldCamera = Camera.main;
     }
 
     private void Update()
     {
-        if (_playerBaseLine != null && Player.Instance != null)
+        if (Player.Instance != null)
         {
             Vector3 playerPos = Player.Instance.transform.position;
+
             Vector3 direction = (transform.position - playerPos).normalized;
             Vector3 point = Utilities.GetScreenEdgePosition(playerPos, direction);
 
-            float horizontalOffset = Mathf.Abs(point.x - playerPos.x)*0.125f;
-            Vector3 targetPos = (Vector2.Distance(point - direction * 1.75f, playerPos) < Vector2.Distance(point - direction * horizontalOffset, playerPos))
-                ? point - direction * 1.75f
-                : point - direction * horizontalOffset;
-
-            //_test1.transform.position = point - direction;
-            //_test2.transform.position = point - direction * horizontalOffset;
-            _playerBaseLine.SetPosition(0, targetPos); // Screen point
-            _playerBaseLine.SetPosition(1, playerPos + direction); // Player point
-
             float alpha = Mathf.Lerp(-0.2f, 0.2f, Vector3.Distance(playerPos, transform.position) * 0.02f);
 
-            _playerBaseLine.colorGradient = new Gradient
-            {
-                colorKeys = new[]
-                {
-                    new GradientColorKey(Color.white, 0)
-                },
-                alphaKeys = new[]
-                {
-                    new GradientAlphaKey(alpha, 0)
-                }
-            };
             _distanceText.text = $"{Vector3.Distance(playerPos, transform.position):F0}m";
             _distanceText.alpha = alpha * 2f;
 
-            _distanceText.transform.position = point - direction;
-        }
-    }
+            _outlinePivot.transform.up = direction;
 
-    private void FixedUpdate()
-    {
-        if (_lineMaterial != null)
-        {
-            _lineMaterial.SetTextureOffset("_MainTex", new Vector2(Time.time * 0.5f, 0));
+            _distanceOutline.color = new Color(1, 1, 1, alpha);
+
+            _distanceText.transform.position = point - direction * 1.5f;
         }
     }
 
